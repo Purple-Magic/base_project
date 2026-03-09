6f5df8609ef6ebb88a9ba0c16762ca90fd4fabb0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2026-03-09 10:13:20 +0400

    Update database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 1) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_09_054448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "solid_cable_messages", force: :cascade do |t|
-    t.binary "channel", null: false
-    t.bigint "channel_hash", null: false
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.binary "payload", null: false
-    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
-    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
-    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "updated_at", null: false
   end
 end
